8e47e517c8912dbb68b7368180b90c7513c4840f
Sascha <sascha.heinze.dev@gmail.com>
2026-01-09 19:21:42 +0100

    feat(urls.py): added a new "info/" - path in the urlpatterns.

--- fruits_app/urls.py
+++ fruits_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from.views import fruits_list
+from.views import fruits_list, info
 
 
 urlpatterns = [
     path('', fruits_list),
+    path('info/', info, name='info'),
 ]
